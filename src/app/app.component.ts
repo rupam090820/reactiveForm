a56cbcd79c5ce520df64555bf0d40343b6fad0f4
rupam090820 <rupams441@gmail.com>
2023-04-22 23:21:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive_form';
+  myForm:any=[];
+constructor(private fbuilder:FormBuilder){
+this.myForm=this.fbuilder.group({
+'name':['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+'email':['',[Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+'pass1':['',[Validators.required]],
+'cpass2':['',[Validators.required]]
+});
+}
+public get name(){
+  return this.myForm.get('name') as FormControl;
+}
+public get email(){
+  return this.myForm.get('email') as FormControl;
+}
+public get pass1(){
+  return this.myForm.get('pass1') as FormControl;
+}
+public get cpass2(){
+  return this.myForm.get('cpass2') as FormControl;
+}
+
+submit(){
+  console.log(this.myForm.value);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ReactiveFormsModule} from'@angular/forms';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
